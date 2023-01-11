6c56b9aa5dd8afb7a5a437fcffae422590e043cf
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-01-11 21:06:43 +0000

    Mapping token to address and address to number of own address

--- src/contracts/ERC20.sol
+++ src/contracts/ERC20.sol
@@ -3,5 +3,11 @@ pragma solidity ^0.8.17;
 
 
 contract ERC21 {
+
+    // Mapping from token id to the owner
+    mapping(uint => address) private = _tokenOwner;
+
+    // Mapping from owner to number of owned tokens
+    mapping(address => uint) private = _OwnedTokensAccount;
     
 }
