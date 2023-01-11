7e1b5c25220185c114a79648fed26e55eef07d3d
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-01-11 21:18:00 +0000

    Created Mint Function

--- src/contracts/ERC20.sol
+++ src/contracts/ERC20.sol
@@ -9,5 +9,13 @@ contract ERC21 {
 
     // Mapping from owner to number of owned tokens
     mapping(address => uint) private = _OwnedTokensAccount;
-    
+
+
+    function _mint(address to, uint256 tokenId) internal {
+        //check for invalid address
+        require(to != address(0), 'ERC721: minting to the zero address');
+        _tokenOwner[tokenId] = to;
+        _OwnedTokensAccount[to] += 1; // _OwnedTokensAccount[to] = _OwnedTokensAccount[0] + 2
+
+    }
 }
