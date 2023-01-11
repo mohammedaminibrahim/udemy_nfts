da5dc78805f0be74aca0a7b668c29c99e97213ce
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-01-11 21:26:23 +0000

    check if token exist and it truthfullness

--- src/contracts/ERC20.sol
+++ src/contracts/ERC20.sol
@@ -10,11 +10,23 @@ contract ERC21 {
     // Mapping from owner to number of owned tokens
     mapping(address => uint) private = _OwnedTokensAccount;
 
+    // check if token id exist and it truthfulness
+    function _exist(uint256 tokenId) internal view returns(bool){
+        // set the address of nft to check the token owner
+        address owner = _tokenOwner[tokenId];
+        // return the truthfullness of the address at the token if
+        return owner != address(0);
+    }
+
 
     function _mint(address to, uint256 tokenId) internal {
         //check for invalid address
         require(to != address(0), 'ERC721: minting to the zero address');
+        // check if token does not exist
+        require(!_exist(tokenId), 'ERC721: Already minted');
+        // adding new address with token id exist
         _tokenOwner[tokenId] = to;
+        // keep track of each address that is minting and adding 1 to it
         _OwnedTokensAccount[to] += 1; // _OwnedTokensAccount[to] = _OwnedTokensAccount[0] + 2
 
     }
