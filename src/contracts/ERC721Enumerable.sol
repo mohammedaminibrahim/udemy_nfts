9c32280fd5fd2beacc994d62d45700927ddb7559
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-01-12 03:02:45 +0000

    Issues on ERC721Enumerable.sol

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -20,7 +20,7 @@ interface ERC721Enumerable is ERC721  {
     // @notice Count NFTs tracked by this contract
     // @return A count of valid NFTs tracked by this contract, where each one of
     //  them has an assigned and queryable owner not equal to the zero address
-    function totalSupply() external view returns (uint256){
+    function totalSupply() public view returns(uint256){
         return _allTokens.length;
     }
 
@@ -43,7 +43,7 @@ interface ERC721Enumerable is ERC721  {
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
 
-        __addTokensToTotalSupply(tokenId);
+        _addTokensToTotalSupply(tokenId);
 
     } 
 
